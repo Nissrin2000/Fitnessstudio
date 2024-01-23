cc85a7e128171eff0368d64c69c569169c3f214d
Nissrin2000 <nissrin.benamar@stud.fra-uas.de>
2024-01-23 17:09:57 +0100

    abgeglichen und CSV eingetragen

--- MitgliedManager.java
+++ MitgliedManager.java
@@ -1,25 +1,37 @@
 import java.util.ArrayList;
 
-public class MitgliedManager { //
-    //Attribute
+public class MitgliedManager { 
+
     public ArrayList<Mitglied> mitgliedListe;
     public ArrayList<Kurs> kursListe;
 
-    //Constructor
+
+    
     public MitgliedManager() {
         mitgliedListe = new ArrayList<Mitglied>();
         kursListe = new ArrayList<Kurs>();
+        String filename = "Mitglieder.csv;
+
     }
 
-    //Methoden
+   
+    
     public void neuesMitgliedAnlegen(int Mitgliednummer, String Name, String Geburtsdatum, String Geschlecht) {
         Mitglied tmpMitglied = new Mitglied(Mitgliednummer, Name, Geburtsdatum, Geschlecht); //tmp=temporär wird nur kurzzeitig in die Liste eingefügt und löscht sich danach von selbst, sobald es in der liste eingefügt wurde wird die kopie gelöscht
         mitgliedListe.add(tmpMitglied);
+        this.mitgliedInCsvSpeichern(newMitglied);
+    }
+
+    private void updateMitglieder() {
+        mitglieder.clear();
+        ArrayList<ArrayList<String>> rawMitglieder = CsvReader.readCSVFile(filename);
+        lstIntoTickets(rawMitglieder);
     }
 
     public void neuenKursAnlegen(int Kursnummer, String Kursname) {
         Kurs tmpKurs = new Kurs(Kursnummer, Kursname); //temp=temporär
         kursListe.add(tmpKurs);
+        this.kursInCsvSpeichern(newKurs)
     }
 
     public void mitgliedLoeschen(Mitglied mitglied) {
@@ -35,5 +47,17 @@ public class MitgliedManager { //
         }
         kursListe.remove(kurs);
     }
+    public void mitgliedInCsvSpeichern(Mitglied mitglied) {
+        Mitglied matchingMitglied = this.getMitgliedWithSameId(Integer.toString(mitglied.getMitgliedId()));
+
+        if (matchingMitglied == null) { // wenn es kein Ticket mit der Id gibt
+            mitglieder.add(mitglied);
+        } else {
+            mitglieder.remove(matchingMitglied);
+            mitglieder.add(mitglied);
+        }
+        CsvReader.updateCsv(filename, mitgliederIntoLst());
+    }
+
 
 }
